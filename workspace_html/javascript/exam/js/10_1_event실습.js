83890e41009bb49e22049daa32e36179380a91b8
최민수 <study.todair@gmail.com>
2025-01-03 09:51:51 +0900

    javascript event : 실습

---
+++ workspace_html/javascript/exam/js/10_1_event실습.js
@@ -0,0 +1,26 @@
+console.log(1651654161651651);
+
+window.addEventListener("load", function(){
+    let login = document.querySelector("#login");
+    console.log('login', login);
+    login.addEventListener('click', function(){
+        console.log("로그인버튼 클릭");
+
+        const id = document.querySelector("#id");
+        console.log(id, id.value)
+
+        const pw = document.querySelector("#pw");
+        console.log(pw, pw.value)
+
+        if( id.value == "admin" && pw.value == 1234){
+            location.href = "http://naver.com";
+        }
+
+        if( id.value == '' ){
+            console.log('id 안썼음')
+            document.querySelector('#login_msg').style.color = '#f00';
+            document.querySelector('#login_msg').textContent = '아이디는 필수입니다';
+        }
+    })
+})
+
