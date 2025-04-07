b5f597a1398a5a224b6f3bdc4544b9c7f1395072
최민수 <study.todair@gmail.com>
2025-04-07 17:32:11 +0900

    Spring - Filter

---
+++ workspace_java/_spring5_filter/src/main/java/kr/or/human5/dto/MemberDTO.java
@@ -0,0 +1,28 @@
+package kr.or.human5.dto;
+
+public class MemberDTO {
+
+	String id;
+	String pw;	// 비밀번호
+	String name;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ workspace_java/_spring5_filter/src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	여긴 너무 비밀스러운 공간이라서<br>
+	로그인 안하면 못들어오는 곳<br>
+	이 화면이 보인다면<br>
+	당신은 회원이군요!!
+</body>
+</html>
